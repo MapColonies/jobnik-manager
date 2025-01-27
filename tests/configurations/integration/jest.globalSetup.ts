4591a222bf917cf5416c5bb6348f6ec5d312b8d3
ronenk1 <ronenk1@rnd-hub.com>
2025-01-27 15:16:06 +0200

    test: generates prisma types

--- tests/configurations/integration/jest.globalSetup.ts
+++ tests/configurations/integration/jest.globalSetup.ts
@@ -11,5 +11,6 @@ export default async function globalSetup(): Promise<void> {
   const connectionDbUrl = createDbConnectUrl(dbConfig);
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const $$ = $({ env: { DATABASE_URL: connectionDbUrl } });
+  // await $$`node_modules/.bin/prisma generate --schema ./src/db/prisma/schema.prisma`;
   await $$`node_modules/.bin/prisma migrate deploy --schema ./src/db/prisma/schema.prisma`;
 }
