ca6b4d0c2eec39df7cf2fba9c90c5606816432ad
netanelC <netanel153@gmail.com>
2025-08-20 19:13:00 +0300

    feat: add docker compose

--- tests/configurations/integration/jest.globalSetup.ts
+++ tests/configurations/integration/jest.globalSetup.ts
@@ -1,9 +1,17 @@
+import path from 'node:path';
+import isPortReachable from 'is-port-reachable';
+import * as compose from 'docker-compose';
 import { $ } from 'zx';
 import type { commonDbFullV1Type } from '@map-colonies/schemas';
 import { initConfig, getConfig } from '../../../src/common/config';
 import { createDbConnectUrl } from '../../../src/db/helpers';
 
 export default async function globalSetup(): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const isDBReachable = await isPortReachable(5432, { host: 'localhost', timeout: 5000 });
+  if (!isDBReachable) {
+    await compose.upAll({ cwd: path.join(__dirname), log: true });
+  }
   await initConfig(true);
   const configInstance = getConfig();
   const dbConfig = configInstance.get('db') as commonDbFullV1Type; // todo - temporary - will removed after dedicated schema with db will be published
--- tests/configurations/integration/jest.globalTeardown.ts
+++ tests/configurations/integration/jest.globalTeardown.ts
@@ -1,3 +1,6 @@
+import path from 'node:path';
+import isCI from 'is-ci';
+import * as compose from 'docker-compose';
 import config from 'config';
 import type { commonDbFullV1Type } from '@map-colonies/schemas';
 import { createConnectionOptions, createPrismaClient } from '../../../src/db/createConnection';
@@ -8,4 +11,7 @@ export default async function globalSetup(): Promise<void> {
   const prisma = createPrismaClient(pgPoolConfig, dbConfig.schema);
   await prisma.$queryRaw`DROP SCHEMA IF EXISTS job_manager CASCADE`;
   await prisma.$disconnect();
+  if (isCI) {
+    await compose.downAll({ cwd: path.join(__dirname), log: true });
+  }
 }
