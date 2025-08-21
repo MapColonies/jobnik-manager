d076fb92bf6cc675c2beb73262e13218b893ccb6
netanelC <netanel153@gmail.com>
2025-08-21 14:17:52 +0300

    test: pr issues

--- tests/configurations/integration/jest.globalSetup.ts
+++ tests/configurations/integration/jest.globalSetup.ts
@@ -1,17 +1,47 @@
+/* eslint-disable no-console */
 import path from 'node:path';
-import isPortReachable from 'is-port-reachable';
+import * as fs from 'fs';
 import * as compose from 'docker-compose';
 import { $ } from 'zx';
 import type { commonDbFullV1Type } from '@map-colonies/schemas';
+import { getPort, checkPort } from 'get-port-please';
 import { initConfig, getConfig } from '../../../src/common/config';
 import { createDbConnectUrl } from '../../../src/db/helpers';
 
-export default async function globalSetup(): Promise<void> {
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const isDBReachable = await isPortReachable(5432, { host: 'localhost', timeout: 5000 });
-  if (!isDBReachable) {
-    await compose.upAll({ cwd: path.join(__dirname), log: true });
+const configPath = path.join(process.cwd(), 'config', 'local-test.json');
+interface Config {
+  db?: {
+    port?: number;
+  };
+}
+
+async function getSelectedPort(config: Config): Promise<number> {
+  let port: number;
+
+  if (config.db?.port !== undefined) {
+    port = config.db.port;
+    console.log(`Found custom port in local-test.json. Using port: ${port}`);
+  } else {
+    // If a port was not defined in the config file, find an available one and write it to local-test.json
+    port = await getPort({ port: 5433 });
+    console.log(`No custom port defined. Using available port: ${port}`);
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    fs.writeFileSync(configPath, JSON.stringify({ ...config, db: { ...config.db, port } }, null, 2), 'utf-8');
   }
+
+  return port;
+}
+
+export default async function globalSetup(): Promise<void> {
+  // Check if the config file exists first
+  const config = fs.existsSync(configPath) ? (JSON.parse(fs.readFileSync(configPath, 'utf-8')) as Config) : {};
+  const port = await getSelectedPort(config);
+
+  if ((await checkPort(port)) === false) {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    await compose.upAll({ cwd: path.join(__dirname), log: true, env: { POSTGRES_PORT: port.toString() } });
+  }
+
   await initConfig(true);
   const configInstance = getConfig();
   const dbConfig = configInstance.get('db') as commonDbFullV1Type; // todo - temporary - will removed after dedicated schema with db will be published
--- tests/configurations/integration/jest.globalTeardown.ts
+++ tests/configurations/integration/jest.globalTeardown.ts
@@ -1,9 +1,16 @@
 import path from 'node:path';
+import * as fs from 'fs';
 import isCI from 'is-ci';
 import * as compose from 'docker-compose';
 import config from 'config';
 import type { commonDbFullV1Type } from '@map-colonies/schemas';
 import { createConnectionOptions, createPrismaClient } from '../../../src/db/createConnection';
+
+interface Config {
+  db: {
+    port: number;
+  };
+}
 
 export default async function globalSetup(): Promise<void> {
   const dbConfig = config.get<commonDbFullV1Type>('db');
@@ -11,7 +18,12 @@ export default async function globalSetup(): Promise<void> {
   const prisma = createPrismaClient(pgPoolConfig, dbConfig.schema);
   await prisma.$queryRaw`DROP SCHEMA IF EXISTS job_manager CASCADE`;
   await prisma.$disconnect();
+
   if (isCI) {
-    await compose.downAll({ cwd: path.join(__dirname), log: true });
+    const configPath = path.join(process.cwd(), 'config', 'local-test.json');
+    const port = (JSON.parse(fs.readFileSync(configPath, 'utf-8')) as Config).db.port;
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    await compose.downAll({ cwd: path.join(__dirname), log: true, env: { POSTGRES_PORT: port.toString() } });
   }
 }
