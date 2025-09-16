4c3e716a12edf020c6c8f6982d514cd46fee9a42
ronenkapelian <72082238+ronenkapelian@users.noreply.github.com>
2025-09-16 17:14:42 +0300

    feat: integrate jobnik manager with config manager & schemas (MAPCO-7924) (#116)
    * feat: integrate jobnik manager with config manager & schemas
    
    * deps: update @map-colonies/schemas dependency to local file reference
    
    * deps: update @map-colonies/schemas to version 1.13.0 and validate cron schedule
    
    * helm: update configmap and values for stale tasks sweeper cron functionality
    
    * helm: update mclabels dependency and adjust references in templates

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,20 @@
 import { Application } from 'express';
 import { DependencyContainer } from 'tsyringe';
-import type { commonDbFullV1Type } from '@map-colonies/schemas';
-import { schedule } from 'node-cron';
+import { schedule, validate } from 'node-cron';
 import { PrismaClient } from '@prismaClient';
 import type { ConfigType } from '@common/config';
 import { registerExternalValues, RegisterOptions } from './containerConfig';
 import { ServerBuilder } from './serverBuilder';
 import { SERVICES } from './common/constants';
 import { verifyDbSetup } from './db/createConnection';
-import { CronConfig } from './common/interfaces';
 import { TaskManager } from './tasks/models/manager';
 
 async function getApp(registerOptions?: RegisterOptions): Promise<[Application, DependencyContainer]> {
   const container = await registerExternalValues(registerOptions);
   const prisma = container.resolve<PrismaClient>(SERVICES.PRISMA);
   const config = container.resolve<ConfigType>(SERVICES.CONFIG);
-  // todo - remove after integrating with config managements
-  const dbConfig = config.get('db') as commonDbFullV1Type;
-  // todo - remove after integrating with config managements
-  const cronConfig = config.get('staleTasksSweeperCron') as CronConfig;
+  const dbConfig = config.get('db');
+  const cronConfig = config.get('staleTasksSweeperCron');
 
   await verifyDbSetup(prisma, dbConfig.schema);
 
@@ -26,6 +22,10 @@ async function getApp(registerOptions?: RegisterOptions): Promise<[Application, 
 
   if (cronConfig.enabled) {
     const taskManager = container.resolve(TaskManager);
+    if (!validate(cronConfig.schedule)) {
+      throw new Error(`Invalid cron schedule: ${cronConfig.schedule}`);
+    }
+
     schedule(cronConfig.schedule, async () => {
       await taskManager.cleanStaleTasks();
     });
