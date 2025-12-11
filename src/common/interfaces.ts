177a45b897b9195fae659e30d29f2a62cb6531b5
ronenk1 <ronenk1@rnd-hub.com>
2025-12-11 16:28:33 +0200

    refactor: remove unused ApiVersionConfig interface from IConfig

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -2,11 +2,3 @@ export interface IConfig {
   get: <T>(setting: string) => T;
   has: (setting: string) => boolean;
 }
-
-export interface ApiVersionConfig {
-  readonly enabled: boolean;
-  readonly filePath: string;
-  readonly basePath: string;
-  readonly rawPath: string;
-  readonly uiPath: string;
-}
